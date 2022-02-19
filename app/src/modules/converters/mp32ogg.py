1c686c60dfe6f3b0f8c41eb11dbd1df22d8eabfe
Oui002 <75451291+Oui002@users.noreply.github.com>
2022-02-19 23:43:49 +0100

    minor changes

--- app/src/modules/converters/mp32ogg.py
+++ app/src/modules/converters/mp32ogg.py
@@ -3,16 +3,16 @@ from os import listdir, remove
 
 def mp32ogg(path: str = "../music"):
     files = listdir(path)
+
     for file in files:
         if not f"{file[:-4]}.ogg" in files:
             if file.endswith('.mp3'):
                 try:
                     AudioSegment.from_mp3(f"{path}/{file}").export(f"{path}/{file[:-4]}.ogg", format="ogg")
                     remove(f"{path}/{file}")
-
                 except:
                     remove(f"{path}/{file}")
                     remove(f"{path}/{file[:-4]}.ogg")
-
+                    
                     pass
                 
